ec180b2cbe7ac482ee3dab9f53b09a475bef7bbe
dzachara <dzachara@ZACHARA-D>
2021-12-16 13:29:28 +0100

    Add project files.

---
+++ L2_7_Zad_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace L2_7_Zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 5;
+
+            if (a == b)
+            {
+                Console.WriteLine($"{a} i {b} są równe.");
+            }
+            else
+            {
+                Console.WriteLine($"{a} i {b} nie są równe.");
+            }
+        }
+    }
+}
